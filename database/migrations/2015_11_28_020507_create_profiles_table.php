83421219fe78bb5f7452b8c007159827a8f8dee2
sbtkzp <d0_n0t_0ften_use_this@yahoo.co.jp>
2015-11-28 12:30:19 +0900

    [tables] プロフィール、TODO登録用テーブル作成

---
+++ database/migrations/2015_11_28_020507_create_profiles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('display_name');
+            $table->string('display_name_en');
+            $table->datetime('birthday');
+            $table->integer('age');
+            $table->string('postal_number');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->string('mobile_phone_number');
+            $table->string('web_site_url');
+            $table->string('blood_type',2);
+            $table->string('memo');
+            $table->string('icon');
+            $table->smallInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('profiles');
+    }
+}
